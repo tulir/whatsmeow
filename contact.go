600a1773e951f20fedff3744fe988b64fa8bc559
Lucas Engelke <L-Engelke@web.de>
2018-06-30 01:29:53 +0200

    implements add / remove member

--- contact.go
+++ contact.go
@@ -31,93 +31,54 @@ func (wac *conn) SubscribePresence(jid string) (<-chan string, error) {
 }
 
 func (wac *conn) CreateGroup(subject string, participants []string) (<-chan string, error) {
-	ts := time.Now().Unix()
-	id := fmt.Sprintf("%d.--%d", ts, wac.msgCount)
-
-	p := buildParticipantNodes(participants)
-
-	//TODO: get proto or improve encoder to handle []interface{}
-	n := binary.Node{
-		Description: "action",
-		Attributes: map[string]string{
-			"type":  "set",
-			"epoch": strconv.Itoa(wac.msgCount),
-		},
-		Content: []interface{}{binary.Node{
-			Description: "group",
-			Attributes: map[string]string{
-				"author": wac.session.Wid,
-				"id": id,
-				"subject": subject,
-				"type": "create",
-			},
-			Content: p,
-		}},
-	}
-
-	wac.msgCount++
-	return wac.writeBinary(n, GROUP, IGNORE, id)
+	return wac.setGroup("create", "", subject, participants)
 }
 
 func (wac *conn) UpdateGroupSubject(subject string, jid string) (<-chan string, error) {
-	ts := time.Now().Unix()
-	id := fmt.Sprintf("%d.--%d", ts, wac.msgCount)
-
-	n := binary.Node{
-		Description: "action",
-		Attributes: map[string]string{
-			"type":  "set",
-			"epoch": strconv.Itoa(wac.msgCount),
-		},
-		Content: []interface{}{binary.Node{
-			Description: "group",
-			Attributes: map[string]string{
-				"author": wac.session.Wid,
-				"id": id,
-				"subject": subject,
-				"jid": jid,
-				"type": "subject",
-			},
-		}},
-	}
-
-	wac.msgCount++
-	return wac.writeBinary(n, GROUP, IGNORE, id)
+	return wac.setGroup("subject", jid, subject, nil)
 }
 
 func (wac *conn) SetAdmin(jid string, participants []string) (<-chan string, error) {
-	ts := time.Now().Unix()
-	id := fmt.Sprintf("%d.--%d", ts, wac.msgCount)
-
-	p := buildParticipantNodes(participants)
-
-	n := binary.Node{
-		Description: "action",
-		Attributes: map[string]string{
-			"type":  "set",
-			"epoch": strconv.Itoa(wac.msgCount),
-		},
-		Content: []interface{}{binary.Node{
-			Description: "group",
-			Attributes: map[string]string{
-				"author": wac.session.Wid,
-				"id": id,
-				"jid": jid,
-				"type": "promote",
-			},
-			Content: p,
-		}},
-	}
-
-	wac.msgCount++
-	return wac.writeBinary(n, GROUP, IGNORE, id)
+	return wac.setGroup("promote", jid, "", participants)
 }
 
 func (wac *conn) RemoveAdmin(jid string, participants []string) (<-chan string, error) {
+	return wac.setGroup("demote", jid, "", participants)
+}
+
+func (wac *conn) AddMember(jid string, participants []string) (<-chan string, error) {
+	return wac.setGroup("add", jid, "", participants)
+}
+
+func (wac *conn) RemoveMember(jid string, participants []string) (<-chan string, error) {
+	return wac.setGroup("remove", jid, "", participants)
+}
+
+func (wac *conn) setGroup(t, jid, subject string, participants []string) (<-chan string, error) {
 	ts := time.Now().Unix()
 	id := fmt.Sprintf("%d.--%d", ts, wac.msgCount)
 
+	//TODO: get proto or improve encoder to handle []interface{}
+
 	p := buildParticipantNodes(participants)
+
+	g := binary.Node{
+		Description: "group",
+		Attributes: map[string]string{
+			"author": wac.session.Wid,
+			"id": id,
+			"type": t,
+		},
+		Content: p,
+	}
+
+	if jid != "" {
+		g.Attributes["jid"] = jid
+	}
+
+	if subject != "" {
+		g.Attributes["subject"] = subject
+	}
 
 	n := binary.Node{
 		Description: "action",
@@ -125,23 +86,19 @@ func (wac *conn) RemoveAdmin(jid string, participants []string) (<-chan string, 
 			"type":  "set",
 			"epoch": strconv.Itoa(wac.msgCount),
 		},
-		Content: []interface{}{binary.Node{
-			Description: "group",
-			Attributes: map[string]string{
-				"author": wac.session.Wid,
-				"id": id,
-				"jid": jid,
-				"type": "demote",
-			},
-			Content: p,
-		}},
+		Content: []interface{}{g},
 	}
 
 	wac.msgCount++
 	return wac.writeBinary(n, GROUP, IGNORE, id)
 }
 
-func buildParticipantNodes(participants []string) (binary.Node){
+func buildParticipantNodes(participants []string) ([]binary.Node) {
+	l := len(participants)
+	if participants == nil || l == 0 {
+		return nil
+	}
+
 	p := make([]binary.Node, len(participants))
 	for i, participant := range participants {
 		p[i] = binary.Node{
