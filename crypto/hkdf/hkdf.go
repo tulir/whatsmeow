da7b8d265d65f3e8b8a0a8cb8354865fb17302c9
Marcel Kocks <marcel.kocks@hotmail.de>
2018-07-12 18:13:29 +0200

    godoc hkdf

--- crypto/hkdf/hkdf.go
+++ crypto/hkdf/hkdf.go
@@ -1,3 +1,11 @@
+/*
+HKDF is a simple key derivation function (KDF) based on
+a hash-based message authentication code (HMAC). It was initially proposed by its authors as a building block in
+various protocols and applications, as well as to discourage the proliferation of multiple KDF mechanisms.
+The main approach HKDF follows is the "extract-then-expand" paradigm, where the KDF logically consists of two modules:
+the first stage takes the input keying material and "extracts" from it a fixed-length pseudorandom key, and then the
+second stage "expands" this key into several additional pseudorandom keys (the output of the KDF).
+*/
 package hkdf
 
 import (
@@ -8,6 +16,9 @@ import (
 	"io"
 )
 
+/*
+Expand expands a given key with the HKDF algorithm.
+*/
 func Expand(key []byte, length int, info string) ([]byte, error) {
 	if info == "" {
 		keyBlock := hmac.New(sha256.New, key)
