afdaf51e34af9e2f7b168aafa39bff38dc55094d
Tulir Asokan <tulir@maunium.net>
2020-03-31 16:25:37 +0300

    Add method for setting group description

--- group.go
+++ group.go
@@ -3,7 +3,10 @@ package whatsapp
 import (
 	"encoding/json"
 	"fmt"
+	"strconv"
 	"time"
+
+	"github.com/Rhymen/go-whatsapp/binary"
 )
 
 func (wac *Conn) GetGroupMetaData(jid string) (<-chan string, error) {
@@ -87,4 +90,68 @@ func (wac *Conn) GroupAcceptInviteCode(code string) (jid string, err error) {
 	}
 
 	return response["gid"].(string), nil
+}
+
+type descriptionID struct {
+	DescID string `json:"descId"`
+}
+
+func (wac *Conn) getDescriptionID(jid string) (string, error) {
+	data, err := wac.GetGroupMetaData(jid)
+	if err != nil {
+		return "none", err
+	}
+	var oldData descriptionID
+	err = json.Unmarshal([]byte(<-data), &oldData)
+	if err != nil {
+		return "none", err
+	}
+	if oldData.DescID == "" {
+		return "none", nil
+	}
+	return oldData.DescID, nil
+}
+
+func (wac *Conn) UpdateGroupDescription(jid, description string) (<-chan string, error) {
+	prevID, err := wac.getDescriptionID(jid)
+	if err != nil {
+		return nil, err
+	}
+	newData := map[string]string{
+		"prev": prevID,
+	}
+	var desc interface{} = description
+	if description == "" {
+		newData["delete"] = "true"
+		desc = nil
+	} else {
+		newData["id"] = fmt.Sprintf("%d-%d", time.Now().Unix(), wac.msgCount*19)
+	}
+	tag := fmt.Sprintf("%d.--%d", time.Now().Unix(), wac.msgCount*19)
+	n := binary.Node{
+		Description: "action",
+		Attributes: map[string]string{
+			"type":  "set",
+			"epoch": strconv.Itoa(wac.msgCount),
+		},
+		Content: []interface{}{
+			binary.Node{
+				Description: "group",
+				Attributes: map[string]string{
+					"id":     tag,
+					"jid":    jid,
+					"type":   "description",
+					"author": wac.Info.Wid,
+				},
+				Content: []binary.Node{
+					{
+						Description: "description",
+						Attributes:  newData,
+						Content:     desc,
+					},
+				},
+			},
+		},
+	}
+	return wac.writeBinary(n, group, 136, tag)
 }
