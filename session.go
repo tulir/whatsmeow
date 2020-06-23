a3e524cf763ecd5cd0422908f8ad7ddce4f4b065
Tulir Asokan <tulir@maunium.net>
2020-06-23 18:56:47 +0300

    Update version number

--- session.go
+++ session.go
@@ -20,7 +20,7 @@ import (
 )
 
 //represents the WhatsAppWeb client version
-var waVersion = []int{0, 4, 2081}
+var waVersion = []int{2, 2025, 6}
 
 /*
 Session contains session individual information. To be able to resume the connection without scanning the qr code
