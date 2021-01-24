2122d6410cec038a93487364fe5fa1848c0c8f60
artem-webdev <37927833+artem-webdev@users.noreply.github.com>
2021-01-24 06:22:11 +0300

    Update read.go

--- read.go
+++ read.go
@@ -83,13 +83,10 @@ func (wac *Conn) processReadData(msgType int, msg []byte) error {
 		// chan string to something like chan map[string]interface{}. The unmarshalling
 		// in several places, especially in session.go, would then be gone.
 		listener <- data[1]
-		
-                close(listener)
-		
+		close(listener)
 		wac.listener.Lock()
 		delete(wac.listener.m, data[0])
 		wac.listener.Unlock()
-		
 	} else if msgType == websocket.BinaryMessage {
 		wac.loginSessionLock.RLock()
 		sess := wac.session
