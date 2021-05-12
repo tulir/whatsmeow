af70f3e6937ab961ebc7a3353b3ee9e5df1f5558
Tulir Asokan <tulir@maunium.net>
2021-05-12 14:56:00 +0300

    Update WhatsApp version identifier

--- session.go
+++ session.go
@@ -17,7 +17,7 @@ import (
 )
 
 //represents the WhatsAppWeb client version
-var waVersion = []int{2, 2104, 9}
+var waVersion = []int{2, 2117, 5}
 
 /*
 Session contains session individual information. To be able to resume the connection without scanning the qr code
