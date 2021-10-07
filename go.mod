217f036546c6830641a5aa09a362a108a1eeee81
Tulir Asokan <tulir@maunium.net>
2021-10-07 12:47:53 +0300

    Print QR codes to terminal

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.16
 require (
 	github.com/RadicalApp/libsignal-protocol-go v0.0.0-20170414202031-d09bcab9f18e
 	github.com/gorilla/websocket v1.4.2
+	github.com/mdp/qrterminal/v3 v3.0.0
 	golang.org/x/crypto v0.0.0-20210506145944-38f3c27a63bf
 	google.golang.org/protobuf v1.26.0
 	maunium.net/go/maulogger/v2 v2.2.4
--- mdtest/main.go
+++ mdtest/main.go
@@ -7,10 +7,13 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
+	"github.com/mdp/qrterminal/v3"
 	log "maunium.net/go/maulogger/v2"
 
 	"go.mau.fi/whatsmeow/multidevice"
@@ -25,8 +28,25 @@ func main() {
 		log.Fatalln("Failed to connect:", err)
 		return
 	}
+	cli.AddEventHandler(handler)
 
 	c := make(chan os.Signal)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 	<-c
+}
+
+func handler(rawEvt interface{}) {
+	switch evt := rawEvt.(type) {
+	case *multidevice.QREvent:
+		go printQRs(evt)
+	}
+}
+
+func printQRs(evt *multidevice.QREvent) {
+	for _, qr := range evt.Codes {
+		fmt.Println("\033[38;2;255;255;255m\u001B[48;2;0;0;0m")
+		qrterminal.GenerateHalfBlock(qr, qrterminal.L, os.Stdout)
+		fmt.Println("\033[0m")
+		time.Sleep(evt.Timeout)
+	}
 }
