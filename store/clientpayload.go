0e67dea7315c0e76f9fd61e37bf2c05b33551ac3
Tulir Asokan <tulir@maunium.net>
2023-05-26 12:14:39 +0300

    Update to web version 2.2322.15

--- store/clientpayload.go
+++ store/clientpayload.go
@@ -74,7 +74,7 @@ func (vc WAVersionContainer) ProtoAppVersion() *waProto.ClientPayload_UserAgent_
 }
 
 // waVersion is the WhatsApp web client version
-var waVersion = WAVersionContainer{2, 2321, 6}
+var waVersion = WAVersionContainer{2, 2322, 15}
 
 // waVersionHash is the md5 hash of a dot-separated waVersion
 var waVersionHash [16]byte
