5fa63e35d8a4e5109af959f86861d15424ef109d
Tulir Asokan <tulir@maunium.net>
2023-07-09 16:46:56 +0300

    wip²

--- pair-code.go
+++ pair-code.go
@@ -16,10 +16,12 @@ import (
 	"regexp"
 	"strconv"
 
+	"golang.org/x/crypto/curve25519"
 	"golang.org/x/crypto/pbkdf2"
 
 	waBinary "go.mau.fi/whatsmeow/binary"
 	"go.mau.fi/whatsmeow/types"
+	"go.mau.fi/whatsmeow/util/hkdfutil"
 	"go.mau.fi/whatsmeow/util/keys"
 )
 
@@ -41,38 +43,32 @@ const (
 var notNumbers = regexp.MustCompile("[^0-9]")
 var linkingBase32 = base32.NewEncoding("123456789ABCDEFGHJKLMNPQRSTVWXYZ")
 
-func encodeLinkingCode(code []byte) string {
-	encoded := linkingBase32.EncodeToString(code)
-	return encoded[0:4] + "-" + encoded[4:]
+type phoneLinkingCache struct {
+	jid         types.JID
+	keyPair     *keys.KeyPair
+	linkingCode string
+	pairingRef  string
 }
 
-type phoneLinkingCache struct {
-	keyPair    *keys.KeyPair
-	pairingRef string
+func pairingRandom(length int) []byte {
+	random := make([]byte, length)
+	_, err := rand.Read(random)
+	if err != nil {
+		panic(err)
+	}
+	return random
 }
 
 func generateCompanionEphemeralKey() (ephemeralKeyPair *keys.KeyPair, ephemeralKey []byte, encodedLinkingCode string) {
 	ephemeralKeyPair = keys.NewKeyPair()
-	firstRandom := make([]byte, 32)
-	secondRandom := make([]byte, 16)
-	linkingCode := make([]byte, 5)
-	_, err := rand.Read(firstRandom[:])
-	if err != nil {
-		panic(err)
-	}
-	_, err = rand.Read(secondRandom[:])
-	if err != nil {
-		panic(err)
-	}
-	_, err = rand.Read(linkingCode[:])
-	if err != nil {
-		panic(err)
-	}
-	encodedLinkingCode = encodeLinkingCode(linkingCode)
+	firstRandom := pairingRandom(32)
+	secondRandom := pairingRandom(16)
+	linkingCode := pairingRandom(5)
+	encodedLinkingCode = linkingBase32.EncodeToString(linkingCode)
 	linkCodeKey := pbkdf2.Key([]byte(encodedLinkingCode), firstRandom, 2<<16, 32, sha256.New)
 	linkCipherBlock, _ := aes.NewCipher(linkCodeKey)
 	encryptedPubkey := ephemeralKeyPair.Pub[:]
-	cipher.NewCTR(linkCipherBlock, make([]byte, 16)).XORKeyStream(encryptedPubkey, encryptedPubkey)
+	cipher.NewCTR(linkCipherBlock, secondRandom).XORKeyStream(encryptedPubkey, encryptedPubkey)
 	ephemeralKey = make([]byte, 80)
 	copy(ephemeralKey[0:32], firstRandom)
 	copy(ephemeralKey[32:48], secondRandom)
@@ -83,6 +79,7 @@ func generateCompanionEphemeralKey() (ephemeralKeyPair *keys.KeyPair, ephemeralK
 func (cli *Client) PairPhone(phone string, showPushNotification bool, clientType PairClientType, clientDisplayName string) (string, error) {
 	ephemeralKeyPair, ephemeralKey, encodedLinkingCode := generateCompanionEphemeralKey()
 	phone = notNumbers.ReplaceAllString(phone, "")
+	jid := types.NewJID(phone, types.DefaultUserServer)
 	resp, err := cli.sendIQ(infoQuery{
 		Namespace: "md",
 		Type:      iqSet,
@@ -90,8 +87,9 @@ func (cli *Client) PairPhone(phone string, showPushNotification bool, clientType
 		Content: []waBinary.Node{{
 			Tag: "link_code_companion_reg",
 			Attrs: waBinary.Attrs{
-				"jid":                           types.NewJID(phone, types.DefaultUserServer),
-				"stage":                         "companion_hello",
+				"jid":   jid,
+				"stage": "companion_hello",
+
 				"should_show_push_notification": strconv.FormatBool(showPushNotification),
 			},
 			Content: []waBinary.Node{
@@ -115,8 +113,10 @@ func (cli *Client) PairPhone(phone string, showPushNotification bool, clientType
 		return "", fmt.Errorf("unexpected type %T in content of link_code_pairing_ref tag", pairingRefNode.Content)
 	}
 	cli.phoneLinkingCache = &phoneLinkingCache{
-		keyPair:    ephemeralKeyPair,
-		pairingRef: string(pairingRef),
+		jid:         jid,
+		keyPair:     ephemeralKeyPair,
+		linkingCode: encodedLinkingCode,
+		pairingRef:  string(pairingRef),
 	}
 	return encodedLinkingCode[0:4] + "-" + encodedLinkingCode[4:], nil
 }
@@ -139,4 +139,53 @@ func (cli *Client) handleCodePairNotification(parentNode *waBinary.Node) error {
 	}
 	wrappedPrimaryEphemeralPub, ok := node.GetChildByTag("link_code_pairing_wrapped_primary_ephemeral_pub").Content.([]byte)
 	primaryIdentityPub, ok := node.GetChildByTag("primary_identity_pub").Content.([]byte)
+
+	newRandom1 := pairingRandom(32)
+	newRandom2 := pairingRandom(32)
+	newRandom3 := pairingRandom(12)
+
+	primaryFirstRandom := wrappedPrimaryEphemeralPub[0:32]
+	primarySecondRandom := wrappedPrimaryEphemeralPub[32:48]
+	primaryEncryptedPubkey := wrappedPrimaryEphemeralPub[48:80]
+
+	linkCodeKey := pbkdf2.Key([]byte(linkCache.linkingCode), primaryFirstRandom, 2<<16, 32, sha256.New)
+	linkCipherBlock, _ := aes.NewCipher(linkCodeKey)
+	primaryDecryptedPubkey := make([]byte, 32)
+	cipher.NewCTR(linkCipherBlock, primarySecondRandom).XORKeyStream(primaryDecryptedPubkey, primaryEncryptedPubkey)
+	ephemeralSharedSecret, err := curve25519.X25519(primaryDecryptedPubkey, linkCache.keyPair.Priv[:])
+	if err != nil {
+		panic(err)
+	}
+	expanded := hkdfutil.SHA256(ephemeralSharedSecret, newRandom2, []byte("link_code_pairing_key_bundle_encryption_key"), 32)
+	concattedKeys := append(append(cli.Store.IdentityKey.Pub[:], primaryIdentityPub...), newRandom1...)
+	expandedBlock, _ := aes.NewCipher(expanded)
+	expandedGCM, _ := cipher.NewGCM(expandedBlock)
+	encryptedKeyBundle := expandedGCM.Seal(nil, newRandom3, concattedKeys, nil)
+	wrappedKeyBundle := append(append(newRandom2, newRandom3...), encryptedKeyBundle...)
+	anotherSharedSecret, err := curve25519.X25519(primaryIdentityPub, cli.Store.IdentityKey.Priv[:])
+	if err != nil {
+		panic(err)
+	}
+	concattedBuffers := append(append(ephemeralSharedSecret, anotherSharedSecret...), newRandom1...)
+	advSecret := hkdfutil.SHA256(concattedBuffers, nil, []byte("adv_secret"), 32)
+	cli.Store.AdvSecretKey = advSecret
+
+	_, err = cli.sendIQ(infoQuery{
+		Namespace: "md",
+		Type:      iqSet,
+		To:        types.ServerJID,
+		Content: []waBinary.Node{{
+			Tag: "link_code_companion_reg",
+			Attrs: waBinary.Attrs{
+				"jid":   linkCache.jid,
+				"stage": "companion_finish",
+			},
+			Content: []waBinary.Node{
+				{Tag: "link_coed_pairing_wrapped_key_bundle", Content: wrappedKeyBundle},
+				{Tag: "companion_identity_public", Content: cli.Store.IdentityKey.Pub[:]},
+				{Tag: "link_code_pairing_ref", Content: linkCodePairingRef},
+			},
+		}},
+	})
+	return err
 }
