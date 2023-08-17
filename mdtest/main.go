1c185f033d88817bcd19fd176975d15b980e18d2
Tulir Asokan <tulir@maunium.net>
2023-08-17 11:18:27 +0300

    Require specifying client type/name separately in PairPhone

--- mdtest/main.go
+++ mdtest/main.go
@@ -183,7 +183,7 @@ func handleCmd(cmd string, args []string) {
 			log.Errorf("Usage: pair-phone <number>")
 			return
 		}
-		linkingCode, err := cli.PairPhone(args[0], true)
+		linkingCode, err := cli.PairPhone(args[0], true, whatsmeow.PairClientFirefox, "Firefox (Linux)")
 		if err != nil {
 			panic(err)
 		}
