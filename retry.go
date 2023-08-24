6da2abde6b7c79a85201fc280b55a2e0240d0977
Tulir Asokan <tulir@maunium.net>
2023-08-24 18:16:50 +0300

    Fix requesting messages from phone

--- retry.go
+++ retry.go
@@ -251,7 +251,7 @@ func (cli *Client) delayedRequestMessageFromPhone(info *types.MessageInfo) {
 	}
 	_, err := cli.SendMessage(
 		ctx,
-		cli.getOwnID(),
+		cli.getOwnID().ToNonAD(),
 		cli.BuildUnavailableMessageRequest(info.Chat, info.Sender, info.ID),
 		SendRequestExtra{Peer: true},
 	)
