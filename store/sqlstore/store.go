da93c942e23a942f7928a91d714457f2c5c92ab9
Angelo Leto <angelo@leto.blue>
2023-10-12 17:25:14 +0200

    rollback of changes from upstream

--- store/sqlstore/store.go
+++ store/sqlstore/store.go
@@ -406,7 +406,7 @@ func (s *SQLStore) putAppStateMutationMACs(tx pgx.Tx, name string, version uint6
 const mutationBatchSize = 400
 
 func (s *SQLStore) PutAppStateMutationMACs(name string, version uint64, mutations []store.AppStateMutationMAC) error {
-	if len(mutations) > mutationBatchSize {
+	if len(mutations) > 0 {
 		tx, err := s.dbPool.Begin(context.Background())
 		if err != nil {
 			return fmt.Errorf("failed to start transaction: %w", err)
@@ -429,18 +429,6 @@ func (s *SQLStore) PutAppStateMutationMACs(name string, version uint64, mutation
 			return fmt.Errorf("failed to commit transaction: %w", err)
 		}
 		return nil
-	} else if len(mutations) > 0 {
-		tx, err := s.dbPool.Begin(context.Background())
-		if err != nil {
-			return fmt.Errorf("failed to start transaction: %w", err)
-		}
-		return s.putAppStateMutationMACs(tx, name, version, mutations)
-		err = tx.Commit(context.Background())
-		if err != nil {
-			_ = tx.Rollback(context.Background())
-			return err
-		}
-
 	}
 	return nil
 }
@@ -607,19 +595,6 @@ func (s *SQLStore) PutAllContactNames(contacts []store.ContactEntry) error {
 				_ = tx.Rollback(context.Background())
 				return err
 			}
-		}
-		err = tx.Commit(context.Background())
-		if err != nil {
-			return fmt.Errorf("failed to commit transaction: %w", err)
-		}
-	} else if len(contacts) > 0 {
-		tx, err := s.dbPool.Begin(context.Background())
-		if err != nil {
-			return fmt.Errorf("failed to start transaction: %w", err)
-		}
-		err = s.putContactNamesBatch(tx, contacts)
-		if err != nil {
-			return err
 		}
 		err = tx.Commit(context.Background())
 		if err != nil {
