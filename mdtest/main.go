74c49f5a7d3110dfb23aafea92b68d6b7bcffe03
Tulir Asokan <tulir@maunium.net>
2024-05-21 18:24:01 +0300

    Update to web version 2.3000.1013652801 and break protobufs
    This includes redoing the protobuf extractor and redirecting legacy
    binary/proto package to the new packages which are shared with FB
    Messenger mode. The backwards compatibility is best effort only, some
    things like field name capitalization changes must be updated manually.

--- mdtest/main.go
+++ mdtest/main.go
@@ -647,7 +647,7 @@ func handleCmd(cmd string, args []string) {
 					GroupInviteMessage: &waProto.GroupInviteMessage{
 						InviteCode:       proto.String(item.AddRequest.Code),
 						InviteExpiration: proto.Int64(item.AddRequest.Expiration.Unix()),
-						GroupJid:         proto.String(jid.String()),
+						GroupJID:         proto.String(jid.String()),
 						GroupName:        proto.String("Test group"),
 						Caption:          proto.String("This is a test group"),
 					},
@@ -777,12 +777,12 @@ func handleCmd(cmd string, args []string) {
 		msg := &waProto.Message{
 			ReactionMessage: &waProto.ReactionMessage{
 				Key: &waProto.MessageKey{
-					RemoteJid: proto.String(recipient.String()),
+					RemoteJID: proto.String(recipient.String()),
 					FromMe:    proto.Bool(fromMe),
-					Id:        proto.String(messageID),
+					ID:        proto.String(messageID),
 				},
 				Text:              proto.String(reaction),
-				SenderTimestampMs: proto.Int64(time.Now().UnixMilli()),
+				SenderTimestampMS: proto.Int64(time.Now().UnixMilli()),
 			},
 		}
 		resp, err := cli.SendMessage(context.Background(), recipient, msg)
@@ -833,12 +833,12 @@ func handleCmd(cmd string, args []string) {
 		}
 		msg := &waProto.Message{ImageMessage: &waProto.ImageMessage{
 			Caption:       proto.String(strings.Join(args[2:], " ")),
-			Url:           proto.String(uploaded.URL),
+			URL:           proto.String(uploaded.URL),
 			DirectPath:    proto.String(uploaded.DirectPath),
 			MediaKey:      uploaded.MediaKey,
 			Mimetype:      proto.String(http.DetectContentType(data)),
-			FileEncSha256: uploaded.FileEncSHA256,
-			FileSha256:    uploaded.FileSHA256,
+			FileEncSHA256: uploaded.FileEncSHA256,
+			FileSHA256:    uploaded.FileSHA256,
 			FileLength:    proto.Uint64(uint64(len(data))),
 		}}
 		resp, err := cli.SendMessage(context.Background(), recipient, msg, whatsmeow.SendRequestExtra{
