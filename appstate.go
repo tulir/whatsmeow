ba3bd05cb8c734962e2af9f479838e7bdb2ad489
Mufid Zukhruf <114892745+mufidz-evermos@users.noreply.github.com>
2024-07-03 11:04:00 +0700

    Update appstate.go
    Co-authored-by: juleve <86941886+juleve@users.noreply.github.com>

--- appstate.go
+++ appstate.go
@@ -69,7 +69,11 @@ func (cli *Client) FetchAppState(name appstate.WAPatchName, fullSync, onlyIfNotS
 			if errors.Is(err, appstate.ErrKeyNotFound) {
 				go cli.requestMissingAppStateKeys(context.TODO(), patches)
 			}
-			return fmt.Errorf("failed to decode app state %s patches for jid %s: %w", name, cli.Store.ID.String(), err)
+             err := fmt.Errorf("failed to decode app state %s patches: %w", name, err)
+            if cli != nil && cli.Store != nil && cli.Store.ID != nil {
+               err = fmt.Errorf("failed to decode app state %s patches for jid %s: %w", name, cli.Store.ID.String(), err)
+            }
+			return err
 		}
 		wasFullSync := state.Version == 0 && patches.Snapshot != nil
 		state = newState
