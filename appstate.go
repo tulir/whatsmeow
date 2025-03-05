c05ea7b75d63019c82d1a62db9b7c9f364759331
Yee Jia Rong <28086837+fourjr@users.noreply.github.com>
2025-03-05 15:17:02 +0800

    Fix a race condition where mutation.Action is nil and causes a panic error

--- appstate.go
+++ appstate.go
@@ -112,7 +112,7 @@ func (cli *Client) filterContacts(mutations []appstate.Mutation) ([]appstate.Mut
 func (cli *Client) dispatchAppState(mutation appstate.Mutation, fullSync bool, emitOnFullSync bool) {
 	dispatchEvts := !fullSync || emitOnFullSync
 
-	if mutation.Action.ContactAction == nil && mutation.Operation != waServerSync.SyncdMutation_SET {
+	if (mutation.Action != nil && mutation.Action.ContactAction == nil) && mutation.Operation != waServerSync.SyncdMutation_SET {
 		return
 	}
 
