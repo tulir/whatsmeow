c13c74800b61ad5039769a97f7fc883129fcdfa0
Tulir Asokan <tulir@maunium.net>
2025-04-09 12:13:07 +0300

    sqlstore: mark lid upgrade as incompatible with previous versions

--- store/sqlstore/upgrades/08-lid-mapping.sql
+++ store/sqlstore/upgrades/08-lid-mapping.sql
@@ -1,4 +1,4 @@
--- v8 (compatible with v6+): Add tables for LID<->JID mapping
+-- v8 (compatible with v8+): Add tables for LID<->JID mapping
 CREATE TABLE whatsmeow_lid_map (
 	lid TEXT PRIMARY KEY,
 	pn  TEXT UNIQUE NOT NULL
