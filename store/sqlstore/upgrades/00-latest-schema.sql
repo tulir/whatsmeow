55e80223f7407f0b9fc5f149e5fe1d0409ee8c8d
Adam Van Ymeren <adam@vany.ca>
2025-05-20 14:41:06 -0700

    Add missing event_buffer from v9 schema to latest_schema

--- store/sqlstore/upgrades/00-latest-schema.sql
+++ store/sqlstore/upgrades/00-latest-schema.sql
@@ -141,3 +141,13 @@ CREATE TABLE whatsmeow_lid_map (
 	lid TEXT PRIMARY KEY,
 	pn  TEXT UNIQUE NOT NULL
 );
+
+CREATE TABLE whatsmeow_event_buffer (
+	our_jid          TEXT   NOT NULL,
+	ciphertext_hash  bytea  NOT NULL CHECK ( length(ciphertext_hash) = 32 ),
+	plaintext        bytea,
+	server_timestamp BIGINT NOT NULL,
+	insert_timestamp BIGINT NOT NULL,
+	PRIMARY KEY (our_jid, ciphertext_hash),
+	FOREIGN KEY (our_jid) REFERENCES whatsmeow_device(jid) ON DELETE CASCADE ON UPDATE CASCADE
+);
