0e49567129b236fd561c8c2b9095ab26d26baa94
yangchaosen <847879871@qq.com>
2025-06-04 17:02:33 +0800

    I've updated the privacy settings for "Selected contact blacklist" and can now access full privacy details.

--- privacysettings.go
+++ privacysettings.go
@@ -61,11 +61,29 @@ func (cli *Client) GetPrivacySettings(ctx context.Context) (settings types.Priva
 // SetPrivacySetting will set the given privacy setting to the given value.
 // The privacy settings will be fetched from the server after the change and the new settings will be returned.
 // If an error occurs while fetching the new settings, will return an empty struct.
-func (cli *Client) SetPrivacySetting(ctx context.Context, name types.PrivacySettingType, value types.PrivacySetting) (settings types.PrivacySettings, err error) {
+func (cli *Client) SetPrivacySetting(ctx context.Context, name types.PrivacySettingType, value types.PrivacySetting, userJID []types.PrivacyJID, DHash int64) (settings types.PrivacySettings, err error) {
 	settingsPtr, err := cli.TryFetchPrivacySettings(ctx, false)
 	if err != nil {
 		return settings, err
 	}
+
+	attr := waBinary.Attrs{
+		"name":  string(name),
+		"value": string(value),
+	}
+
+	userNodes := make([]waBinary.Node, len(userJID))
+	if value == types.PrivacySettingContactBlacklist && len(userJID) > 0 {
+		for i, item := range userJID {
+			userNodes[i].Tag = "user"
+			userNodes[i].Attrs = waBinary.Attrs{"action": string(item.Action), "jid": item.JID.ToNonAD().String()}
+		}
+	}
+
+	if DHash > 0 {
+		attr["dhash"] = DHash
+	}
+
 	_, err = cli.sendIQ(infoQuery{
 		Namespace: "privacy",
 		Type:      iqSet,
@@ -73,36 +91,119 @@ func (cli *Client) SetPrivacySetting(ctx context.Context, name types.PrivacySett
 		Content: []waBinary.Node{{
 			Tag: "privacy",
 			Content: []waBinary.Node{{
-				Tag: "category",
-				Attrs: waBinary.Attrs{
-					"name":  string(name),
-					"value": string(value),
-				},
+				Tag:     "category",
+				Attrs:   attr,
+				Content: userNodes,
 			}},
 		}},
 	})
 	if err != nil {
 		return settings, err
 	}
+
+	var (
+		dhash int64
+		jids  []types.JID
+	)
+	if value == types.PrivacySettingContactBlacklist {
+		jids, dhash = cli.GetPrivacyContactBlacklist(ctx, name)
+		//<iq from="s.whatsapp.net" id="221.99-345" type="result"><privacy><category dhash="1749023801911" name="profile" value="contact_blacklist"/></privacy></iq>
+	}
+
 	settings = *settingsPtr
 	switch name {
 	case types.PrivacySettingTypeGroupAdd:
-		settings.GroupAdd = value
+		settings.GroupAdd = types.PrivacySettingItem{
+			Value: value,
+			JID:   jids,
+			DHash: dhash,
+		}
 	case types.PrivacySettingTypeLastSeen:
-		settings.LastSeen = value
+		settings.LastSeen = types.PrivacySettingItem{
+			Value: value,
+			JID:   jids,
+			DHash: dhash,
+		}
+
 	case types.PrivacySettingTypeStatus:
-		settings.Status = value
+		settings.Status = types.PrivacySettingItem{
+			Value: value,
+			JID:   jids,
+			DHash: dhash,
+		}
 	case types.PrivacySettingTypeProfile:
-		settings.Profile = value
+		settings.Profile = types.PrivacySettingItem{
+			Value: value,
+			JID:   jids,
+			DHash: dhash,
+		}
 	case types.PrivacySettingTypeReadReceipts:
-		settings.ReadReceipts = value
+		settings.ReadReceipts = types.PrivacySettingItem{
+			Value: value,
+			JID:   jids,
+			DHash: dhash,
+		}
 	case types.PrivacySettingTypeOnline:
-		settings.Online = value
+		settings.Online = types.PrivacySettingItem{
+			Value: value,
+			JID:   jids,
+			DHash: dhash,
+		}
 	case types.PrivacySettingTypeCallAdd:
-		settings.CallAdd = value
+		settings.CallAdd = types.PrivacySettingItem{
+			Value: value,
+			JID:   jids,
+			DHash: dhash,
+		}
 	}
 	cli.privacySettingsCache.Store(&settings)
 	return
+}
+
+func (cli *Client) GetPrivacyContactBlacklist(ctx context.Context, tag types.PrivacySettingType) (list []types.JID, DHash int64) {
+	attrs := waBinary.Attrs{"name": string(tag), "value": string(types.PrivacySettingContactBlacklist)}
+
+	nodes := []waBinary.Node{{
+		Tag: "privacy",
+		Content: []waBinary.Node{
+			{
+				Tag:   "list",
+				Attrs: attrs,
+			},
+		},
+	}}
+
+	resp, err := cli.sendIQ(infoQuery{
+		Namespace: "privacy",
+		Type:      iqGet,
+		Context:   ctx,
+		To:        types.ServerJID,
+		Content:   nodes,
+	})
+	if err != nil {
+		return nil, 0
+	}
+
+	node, ok := resp.GetOptionalChildByTag("list")
+	if !ok {
+		return nil, 0
+	}
+
+	dhashValue, ok := node.AttrGetter().GetInt64("dhash", true)
+	if ok {
+		DHash = dhashValue
+	}
+
+	userList := node.GetChildrenByTag("user")
+	list = make([]types.JID, len(userList))
+	for v, i := range userList {
+		jid, ok := i.AttrGetter().GetJID("jid", true)
+		if ok {
+			list[v] = jid
+		}
+	}
+
+	return list, DHash
 }
 
 // SetDefaultDisappearingTimer will set the default disappearing message timer.
@@ -130,27 +231,64 @@ func (cli *Client) parsePrivacySettings(privacyNode *waBinary.Node, settings *ty
 		ag := child.AttrGetter()
 		name := types.PrivacySettingType(ag.String("name"))
 		value := types.PrivacySetting(ag.String("value"))
+
+		var (
+			list  []types.JID
+			dhash int64
+		)
+		if value == types.PrivacySettingContactBlacklist {
+			list, dhash = cli.GetPrivacyContactBlacklist(context.Background(), name)
+		}
+
 		switch name {
 		case types.PrivacySettingTypeGroupAdd:
-			settings.GroupAdd = value
+			settings.GroupAdd = types.PrivacySettingItem{
+				Value: value,
+				DHash: dhash,
+				JID:   list,
+			}
 			evt.GroupAddChanged = true
 		case types.PrivacySettingTypeLastSeen:
-			settings.LastSeen = value
+			settings.LastSeen = types.PrivacySettingItem{
+				Value: value,
+				DHash: dhash,
+				JID:   list,
+			}
 			evt.LastSeenChanged = true
 		case types.PrivacySettingTypeStatus:
-			settings.Status = value
+			settings.Status = types.PrivacySettingItem{
+				Value: value,
+				DHash: dhash,
+				JID:   list,
+			}
 			evt.StatusChanged = true
 		case types.PrivacySettingTypeProfile:
-			settings.Profile = value
+			settings.Profile = types.PrivacySettingItem{
+				Value: value,
+				DHash: dhash,
+				JID:   list,
+			}
 			evt.ProfileChanged = true
 		case types.PrivacySettingTypeReadReceipts:
-			settings.ReadReceipts = value
+			settings.ReadReceipts = types.PrivacySettingItem{
+				Value: value,
+				DHash: dhash,
+				JID:   list,
+			}
 			evt.ReadReceiptsChanged = true
 		case types.PrivacySettingTypeOnline:
-			settings.Online = value
+			settings.Online = types.PrivacySettingItem{
+				Value: value,
+				DHash: dhash,
+				JID:   list,
+			}
 			evt.OnlineChanged = true
 		case types.PrivacySettingTypeCallAdd:
-			settings.CallAdd = value
+			settings.CallAdd = types.PrivacySettingItem{
+				Value: value,
+				DHash: dhash,
+				JID:   list,
+			}
 			evt.CallAddChanged = true
 		}
 	}
