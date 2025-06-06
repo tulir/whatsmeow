6110de3471ab179b201e7c7be156aff5803f5fc6
Tulir Asokan <tulir@maunium.net>
2025-06-06 12:47:02 +0300

    message: add id to decryption error log

--- message.go
+++ message.go
@@ -342,7 +342,7 @@ func (cli *Client) decryptMessages(ctx context.Context, info *types.MessageInfo,
 			cli.Log.Debugf("Ignoring message %s from %s: %v", info.ID, info.SourceString(), err)
 			return
 		} else if err != nil {
-			cli.Log.Warnf("Error decrypting message from %s: %v", info.SourceString(), err)
+			cli.Log.Warnf("Error decrypting message %s from %s: %v", info.ID, info.SourceString(), err)
 			isUnavailable := encType == "skmsg" && !containsDirectMsg && errors.Is(err, signalerror.ErrNoSenderKeyForUser)
 			if encType != "msmsg" {
 				go cli.sendRetryReceipt(context.WithoutCancel(ctx), node, info, isUnavailable)
