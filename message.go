9948ada1f8aa7083cd47ec787f2edb93d5e7a302
Tulir Asokan <tulir@maunium.net>
2025-06-27 15:33:20 +0200

    message: also fix RecipientAlt

--- message.go
+++ message.go
@@ -107,8 +107,8 @@ func (cli *Client) parseMessageSource(node *waBinary.Node, requireParticipant bo
 		} else {
 			source.Chat = from.ToNonAD()
 		}
-		if source.AddressingMode == types.AddressingModeLID {
-			source.RecipientAlt = ag.OptionalJIDOrEmpty("peer_recipient_pn") // existence of this field is not confirmed
+		if source.Chat.Server == types.HiddenUserServer {
+			source.RecipientAlt = ag.OptionalJIDOrEmpty("peer_recipient_pn")
 		} else {
 			source.RecipientAlt = ag.OptionalJIDOrEmpty("peer_recipient_lid")
 		}
