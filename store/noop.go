5ccbdbda35f84cd7bb719ccbc23c1553b4adfc6d
Tulir Asokan <tulir@maunium.net>
2025-07-23 11:23:37 +0300

    msgsecret: get real sender from database (#881)

--- store/noop.go
+++ store/noop.go
@@ -199,8 +199,8 @@ func (n *NoopStore) PutMessageSecret(ctx context.Context, chat, sender types.JID
 	return n.Error
 }
 
-func (n *NoopStore) GetMessageSecret(ctx context.Context, chat, sender types.JID, id types.MessageID) ([]byte, error) {
-	return nil, n.Error
+func (n *NoopStore) GetMessageSecret(ctx context.Context, chat, sender types.JID, id types.MessageID) ([]byte, types.JID, error) {
+	return nil, types.EmptyJID, n.Error
 }
 
 func (n *NoopStore) PutPrivacyTokens(ctx context.Context, tokens ...PrivacyToken) error {
