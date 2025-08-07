72ce90b82194914003618b93ee4c8dd80c9c9a30
Tulir Asokan <tulir@maunium.net>
2025-08-07 10:21:45 +0300

    types/events: fix potential nil pointer

--- types/events/events.go
+++ types/events/events.go
@@ -386,7 +386,7 @@ func (evt *Message) UnwrapRaw() *Message {
 		evt.Message = evt.Message.GetEditedMessage().GetMessage()
 		evt.IsEdit = true
 	}
-	if evt.Message.MessageContextInfo == nil && evt.RawMessage.MessageContextInfo != nil {
+	if evt.Message != nil && evt.RawMessage != nil && evt.Message.MessageContextInfo == nil && evt.RawMessage.MessageContextInfo != nil {
 		evt.Message.MessageContextInfo = evt.RawMessage.MessageContextInfo
 	}
 	return evt
