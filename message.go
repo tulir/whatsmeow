4fe97da162dcfd678ed47bbdb8ecbee0307f72ca
Tulir Asokan <tulir@maunium.net>
2025-10-05 11:30:24 +0300

    message: send ack on decryption error

--- message.go
+++ message.go
@@ -379,10 +379,13 @@ func (cli *Client) decryptMessages(ctx context.Context, info *types.MessageInfo,
 				return
 			}
 			isUnavailable := encType == "skmsg" && !containsDirectMsg && errors.Is(err, signalerror.ErrNoSenderKeyForUser)
+			// TODO this probably isn't supposed to ack
 			if cli.SynchronousAck {
 				cli.sendRetryReceipt(ctx, node, info, isUnavailable)
+				cli.sendAck(node, 0)
 			} else {
 				go cli.sendRetryReceipt(context.WithoutCancel(ctx), node, info, isUnavailable)
+				go cli.sendAck(node, 0)
 			}
 			cli.dispatchEvent(&events.UndecryptableMessage{
 				Info:            *info,
