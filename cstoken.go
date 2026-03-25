033067008d7dee2f541f533dd7df82628d7afab2
Gustavo Quadri <87215048+gusquadri@users.noreply.github.com>
2026-03-25 02:19:47 -0300

    fix: copyright header

--- cstoken.go
+++ cstoken.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2026 Gus Quadri
+// Copyright (c) 2026 Tulir Asokan
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
