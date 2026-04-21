5b8886176ff74c96368e7e5e34248cba113b567c
Tulir Asokan <tulir@maunium.net>
2026-04-21 11:15:47 +0300

    clientpayload: bump version number to v1037753511

--- store/clientpayload.go
+++ store/clientpayload.go
@@ -76,7 +76,7 @@ func (vc WAVersionContainer) ProtoAppVersion() *waWa6.ClientPayload_UserAgent_Ap
 }
 
 // waVersion is the WhatsApp web client version
-var waVersion = WAVersionContainer{2, 3000, 1037076227}
+var waVersion = WAVersionContainer{2, 3000, 1037753511}
 
 // waVersionHash is the md5 hash of a dot-separated waVersion
 var waVersionHash [16]byte
