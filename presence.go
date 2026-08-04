1494ba731cb085199b062ec691624666e5593074
Tulir Asokan <tulir@maunium.net>
2026-08-04 17:48:12 +0300

    presence: remove from attribute in chat state sending

--- presence.go
+++ presence.go
@@ -128,10 +128,6 @@ func (cli *Client) SubscribePresence(ctx context.Context, jid types.JID) error {
 //
 // The media parameter can be set to indicate the user is recording media (like a voice message) rather than typing a text message.
 func (cli *Client) SendChatPresence(ctx context.Context, jid types.JID, state types.ChatPresence, media types.ChatPresenceMedia) error {
-	ownID := cli.getOwnID()
-	if ownID.IsEmpty() {
-		return ErrNotLoggedIn
-	}
 	content := []waBinary.Node{{Tag: string(state)}}
 	if state == types.ChatPresenceComposing && len(media) > 0 {
 		content[0].Attrs = waBinary.Attrs{
@@ -141,8 +137,7 @@ func (cli *Client) SendChatPresence(ctx context.Context, jid types.JID, state ty
 	return cli.sendNode(ctx, waBinary.Node{
 		Tag: "chatstate",
 		Attrs: waBinary.Attrs{
-			"from": ownID,
-			"to":   jid,
+			"to": jid,
 		},
 		Content: content,
 	})
